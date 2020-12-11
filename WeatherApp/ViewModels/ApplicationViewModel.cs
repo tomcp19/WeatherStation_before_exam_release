4fe6fb96c2b66dd788360990fd41da3c3fb573ce
Nick B <nbourrereg@hotmail.com>
2020-12-11 15:33:53 -0500

    Add project files.

---
+++ WeatherApp/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,178 @@
+﻿using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeatherApp.Commands;
+using WeatherApp.Services;
+
+namespace WeatherApp.ViewModels
+{
+    public class ApplicationViewModel : BaseViewModel
+    {
+        #region Membres
+
+        private BaseViewModel currentViewModel;
+        private List<BaseViewModel> viewModels;
+        private TemperatureViewModel tvm;
+        private OpenWeatherService ows;
+        private string filename;
+
+        private VistaSaveFileDialog saveFileDialog;
+        private VistaOpenFileDialog openFileDialog;
+
+        #endregion
+
+        #region Propriétés
+        /// <summary>
+        /// Model actuellement affiché
+        /// </summary>
+        public BaseViewModel CurrentViewModel
+        {
+            get { return currentViewModel; }
+            set {
+                currentViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// String contenant le nom du fichier
+        /// </summary>
+        public string Filename
+        {
+            get
+            {
+                return filename;
+            }
+            set
+            {
+                filename = value;
+            }
+        }
+
+        /// <summary>
+        /// Commande pour changer la page à afficher
+        /// </summary>
+        public DelegateCommand<string> ChangePageCommand { get; set; }
+
+
+        public List<BaseViewModel> ViewModels
+        {
+            get {
+                if (viewModels == null)
+                    viewModels = new List<BaseViewModel>();
+                return viewModels;
+            }
+        }
+        #endregion
+
+        public ApplicationViewModel()
+        {
+            ChangePageCommand = new DelegateCommand<string>(ChangePage);
+
+            initViewModels();
+
+            CurrentViewModel = ViewModels[0];
+
+        }
+
+        #region Méthodes
+        void initViewModels()
+        {
+            /// TemperatureViewModel setup
+            tvm = new TemperatureViewModel();
+
+            string apiKey = "";
+
+            if (Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "DEVELOPMENT")
+            {
+                apiKey = AppConfiguration.GetValue("OWApiKey");
+            }
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.apiKey) && apiKey == "")
+            {
+                tvm.RawText = "Aucune clé API, veuillez la configurer";
+            } else
+            {
+                if (apiKey == "")
+                    apiKey = Properties.Settings.Default.apiKey;
+
+                ows = new OpenWeatherService(apiKey);
+            }
+
+            tvm.SetTemperatureService(ows);
+            ViewModels.Add(tvm);
+
+            var cvm = new ConfigurationViewModel();
+            ViewModels.Add(cvm);
+        }
+
+
+
+        private void ChangePage(string pageName)
+        {
+            if (CurrentViewModel is ConfigurationViewModel)
+            {
+                ows.SetApiKey(Properties.Settings.Default.apiKey);
+
+                var vm = (TemperatureViewModel)ViewModels.FirstOrDefault(x => x.Name == typeof(TemperatureViewModel).Name);
+                if (vm.TemperatureService == null)
+                    vm.SetTemperatureService(ows);
+            }
+
+            CurrentViewModel = ViewModels.FirstOrDefault(x => x.Name == pageName);
+        }
+
+        private bool CanExport(string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Méthode qui exécute l'exportation
+        /// </summary>
+        /// <param name="obj"></param>
+        private void Export(string obj)
+        {
+
+            if (saveFileDialog == null)
+            {
+                saveFileDialog = new VistaSaveFileDialog();
+                saveFileDialog.Filter = "Json file|*.json|All files|*.*";
+                saveFileDialog.DefaultExt = "json";
+            }
+
+        }
+
+        private void saveToFile()
+        {
+
+
+        }
+
+        private void openFromFile()
+        {
+
+        }
+
+        private void Import(string obj)
+        {
+            if (openFileDialog == null)
+            {
+                openFileDialog = new VistaOpenFileDialog();
+                openFileDialog.Filter = "Json file|*.json|All files|*.*";
+                openFileDialog.DefaultExt = "json";
+
+
+            }
+        }
+
+        private void ChangeLanguage(string language)
+        {
+
+        }
+
+        #endregion
+        
+    }
+}
---
+++ WeatherApp/ViewModels/TemperatureViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using WeatherApp.Commands;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewModels
+{
+    public class TemperatureViewModel : BaseViewModel
+    {
+        private TemperatureModel currentTemp;
+        private string city;
+
+        public ITemperatureService TemperatureService { get; private set; }
+
+        public DelegateCommand<string> GetTempCommand { get; set; }
+
+        public TemperatureModel CurrentTemp 
+        { 
+            get => currentTemp;
+            set
+            {
+                currentTemp = value;
+                OnPropertyChanged();
+                OnPropertyChanged("RawText");
+            }
+        }
+
+        private ObservableCollection<TemperatureModel> temperatures;
+
+        public ObservableCollection<TemperatureModel> Temperatures
+        {
+            get { return temperatures; }
+            set { 
+                temperatures = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                city = value;
+
+                if (TemperatureService != null)
+                {
+                    TemperatureService.SetLocation(City);
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        private string _rawText;
+
+        public string RawText {
+            get {
+                return _rawText;
+            }
+            set
+            {
+                _rawText = value;
+                OnPropertyChanged();
+            }
+            
+        }
+
+        public TemperatureViewModel()
+        {
+            Name = this.GetType().Name;
+            Temperatures = new ObservableCollection<TemperatureModel>();
+
+            GetTempCommand = new DelegateCommand<string>(GetTemp, CanGetTemp);
+        }
+
+
+        public bool CanGetTemp(string obj)
+        {
+            return TemperatureService != null;
+        }
+
+        public void GetTemp(string obj)
+        {
+            if (TemperatureService == null) throw new NullReferenceException();
+
+            _ = GetTempAsync();
+        }
+
+        private async Task GetTempAsync()
+        {
+            CurrentTemp = await TemperatureService.GetTempAsync();
+
+            if (CurrentTemp != null)
+            {
+                RawText = CurrentTemp.ToString() + Environment.NewLine + RawText;
+                Debug.WriteLine(CurrentTemp);
+            }
+        }
+
+        public double CelsiusInFahrenheit(double c)
+        {
+            return c * 9.0 / 5.0 + 32;
+        }
+
+        public double FahrenheitInCelsius(double f)
+        {
+            return (f - 32) * 5.0 / 9.0;
+        }
+
+        public void SetTemperatureService(ITemperatureService srv)
+        {
+            TemperatureService = srv;
+        }
+    }
+}
